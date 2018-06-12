93ec6cc24b103311172cbdb8a2172abd527ba4dc
Lucas <lucas.bremond@gmail.com>
2018-06-11 20:23:42 -0700

    [misc] Testing .travis.yml

---
+++ tools/docker/environment/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+################################################################################################################################################################
+
+# @project        Library/Core
+# @file           tools/docker/environment/run.sh
+# @author         Lucas Brémond <lucas@loftorbital.com>
+# @license        TBD
+
+################################################################################################################################################################
+
+script_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "${script_directory}/../setup.sh"
+
+docker run \
+--name="${container_name}" \
+-it \
+--rm \
+--privileged \
+--volume="${project_directory}:/${image_name}:rw" \
+--volume="/${image_name}/build" \
+--volume="${script_directory}/helpers/build.sh:/${image_name}/build/build.sh:ro" \
+--volume="${script_directory}/helpers/test.sh:/${image_name}/build/test.sh:ro" \
+--volume="${script_directory}/helpers/debug.sh:/${image_name}/build/debug.sh:ro" \
+--volume="${script_directory}/helpers/clean.sh:/${image_name}/build/clean.sh:ro" \
+--workdir="/${image_name}/build" \
+"${image_name}" \
+"./build.sh && ./test.sh"
+
+################################################################################################################################################################
