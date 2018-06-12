6726367a98333f65f791106000aa42addf739ec4
Lucas <lucas.bremond@gmail.com>
2018-06-11 23:53:31 -0700

    [misc] Testing .travis.yml

--- tools/ci/test.sh
+++ tools/ci/test.sh
@@ -11,16 +11,19 @@
 
 script_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-project_directory="${script_directory}"/../..
+project_directory="${script_directory}/../.."
+docker_directory="${script_directory}/../docker"
+
+source "${docker_directory}/.env"
 
 docker run \
 --rm \
 --volume="${project_directory}:/app:rw" \
 --volume="/app/build" \
---volume="${project_directory}/tools/docker/environment/helpers/build.sh:/app/build/build.sh:ro" \
---volume="${project_directory}/tools/docker/environment/helpers/test.sh:/app/build/test.sh:ro" \
+--volume="${docker_directory}/environment/helpers/build.sh:/app/build/build.sh:ro" \
+--volume="${docker_directory}/environment/helpers/test.sh:/app/build/test.sh:ro" \
 --workdir="/app/build" \
-"openspacecollective/library-core:latest" \
-/bin/bash -c "/app/build/build.sh"
+${image_name} \
+/bin/bash -c "/app/build/build.sh && /app/build/test.sh"
 
 ################################################################################################################################################################
