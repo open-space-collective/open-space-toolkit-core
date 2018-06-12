9386cf283497a1355b9f809dc5b29091586cc9f3
Lucas <lucas.bremond@gmail.com>
2018-06-12 11:15:39 -0700

    [misc] Test CI

---
+++ tools/ci/documentation.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+################################################################################################################################################################
+
+# @project        Library/Core
+# @file           tools/ci/documentation.sh
+# @author         Lucas Brémond <lucas@loftorbital.com>
+# @license        TBD
+
+################################################################################################################################################################
+
+script_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+project_directory="${script_directory}/../.."
+docker_directory="${script_directory}/../docker"
+
+source "${docker_directory}/.env"
+
+docker run \
+--rm \
+--volume="${project_directory}:/app:rw" \
+--volume="/app/build" \
+--workdir="/app/build" \
+${image_name} \
+/bin/bash -c "cmake -BUILD_DOCUMENTATION=ON .. && make docs"
+
+################################################################################################################################################################
