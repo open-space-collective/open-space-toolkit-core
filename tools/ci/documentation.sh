fc62aafab533fd77210c3ef918986ea878635af4
Lucas <lucas.bremond@gmail.com>
2018-06-12 19:13:30 -0700

    [misc] Test CI

--- tools/ci/documentation.sh
+++ tools/ci/documentation.sh
@@ -60,54 +60,54 @@ mv git .git
 
 ls -la
 
-# cp -r "${project_directory}"/docs/* .
+cp -r "${project_directory}"/docs/* .
 
 # Need to create a .nojekyll file to allow filenames starting with an underscore
 # to be seen on the gh-pages site. Therefore creating an empty .nojekyll file.
 # Presumably this is only needed when the SHORT_NAMES option in Doxygen is set
 # to NO, which it is by default. So creating the file just in case.
 
-# ls -la
+ls -la
 
-# echo "" > .nojekyll
+echo "" > .nojekyll
 
 git status
 
-# # Only upload if Doxygen successfully created the documentation.
-# # Check this by verifying that the html directory and the file html/index.html
-# # both exist. This is a good indication that Doxygen did its work.
+# Only upload if Doxygen successfully created the documentation.
+# Check this by verifying that the html directory and the file html/index.html
+# both exist. This is a good indication that Doxygen did its work.
 
-# if [ -d "html" ] && [ -f "html/index.html" ]; then
+if [ -d "html" ] && [ -f "html/index.html" ]; then
 
-#     # Add everything in this directory (the Doxygen code documentation) to the gh-pages branch.
-#     # GitHub is smart enough to know which files have changed and which files have
-#     # stayed the same and will only update the changed files.
+    # Add everything in this directory (the Doxygen code documentation) to the gh-pages branch.
+    # GitHub is smart enough to know which files have changed and which files have
+    # stayed the same and will only update the changed files.
 
-#     echo 'Adding documentation to the gh-pages branch...'
+    echo 'Adding documentation to the gh-pages branch...'
     
-#     git add --all
+    git add --all
 
-#     # Commit the added files with a title and description containing the Travis CI
-#     # build number and the GitHub commit reference that issued this build.
+    # Commit the added files with a title and description containing the Travis CI
+    # build number and the GitHub commit reference that issued this build.
     
-#     git commit -m "[feature] Deploy documentation to GitHub Pages" -m "Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
+    git commit -m "[feature] Deploy documentation to GitHub Pages" -m "Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
 
-#     # Force push to the remote gh-pages branch.
-#     # The ouput is redirected to /dev/null to hide any sensitive credential data that might otherwise be exposed.
+    # Force push to the remote gh-pages branch.
+    # The ouput is redirected to /dev/null to hide any sensitive credential data that might otherwise be exposed.
 
-#     echo 'Pushing documentation to remote...'
+    echo 'Pushing documentation to remote...'
 
-#     git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}"
-#     # git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
+    git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}"
+    # git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
 
-# else
+else
     
-#     echo '' >&2
-#     echo 'Warning: No documentation (html) files have been found!' >&2
-#     echo 'Warning: Not going to push the documentation to GitHub!' >&2
-#     exit 1
+    echo '' >&2
+    echo 'Warning: No documentation (html) files have been found!' >&2
+    echo 'Warning: Not going to push the documentation to GitHub!' >&2
+    exit 1
 
-# fi
+fi
 
 popd
 
