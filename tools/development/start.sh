d9707217a1e278077d4ea61e890458161f8a9adf
Lucas Brémond <lucas.bremond@gmail.com>
2018-11-16 19:20:25 +0900

    [feature] Update Dockerfile

--- tools/development/start.sh
+++ tools/development/start.sh
@@ -34,14 +34,13 @@ docker run \
 -it \
 --rm \
 --privileged \
---env="cpu_count=${cpu_count}" \
 --volume="${project_directory}:/app:rw" \
 --volume="${script_directory}/helpers/build.sh:/app/build/build.sh:ro" \
 --volume="${script_directory}/helpers/test.sh:/app/build/test.sh:ro" \
 --volume="${script_directory}/helpers/debug.sh:/app/build/debug.sh:ro" \
 --volume="${script_directory}/helpers/clean.sh:/app/build/clean.sh:ro" \
 --workdir="/app/build" \
-"${image_name}" \
+"${image_name}:${image_version}" \
 "/bin/bash"
 
 ################################################################################################################################################################
