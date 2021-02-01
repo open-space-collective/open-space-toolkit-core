e5e1262acdebb7c018f33a72cb2a1401b5f0b93c
Remy Derollez <remyderollez@gmail.com>
2021-02-01 00:13:39 -0800

    dev: added and validated Hello World Julia binding example

--- bindings/julia/src/OpenSpaceToolkitCoreJl.cxx
+++ bindings/julia/src/OpenSpaceToolkitCoreJl.cxx
@@ -7,23 +7,45 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#include <OpenSpaceToolkitCorePy/FileSystem.cpp>
-#include <OpenSpaceToolkitCorePy/Containers.cpp>
-#include <OpenSpaceToolkitCorePy/Types.cpp>
+// #include <OpenSpaceToolkitCorePy/FileSystem.cpp>
+// #include <OpenSpaceToolkitCorePy/Containers.cpp>
+// #include <OpenSpaceToolkitCorePy/Types.cpp>
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include "jlcxx/jlcxx.hpp"
+#include "jlcxx/functions.hpp"
 
-BOOST_PYTHON_MODULE (OpenSpaceToolkitCorePy)
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// C++ code defining classes
+struct World
 {
+  World(const std::string& message = "default hello") : msg(message){}
+  void set(const std::string& msge) { this->msg = msge; }
+  std::string greet() { return msg; }
+  std::string msg;
+  ~World() { std::cout << "Destroying World with message " << msg << std::endl; }
+};
 
-    boost::python::object package = boost::python::scope() ;
-
-    package.attr("__path__") = "ostk" ;
-
-    OpenSpaceToolkitCorePy_Types() ;
-    OpenSpaceToolkitCorePy_Containers() ;
-    OpenSpaceToolkitCorePy_FileSystem() ;
-
+// C++ code to expose classes and their members to Julia in a Module
+JLCXX_MODULE define_julia_module(jlcxx::Module& CppTypes)
+{
+  CppTypes.add_type<World>("World")
+    .constructor<const std::string&>()
+    .method("set", &World::set)
+    .method("greet", &World::greet);
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// BOOST_PYTHON_MODULE (OpenSpaceToolkitCorePy)
+// {
+
+//     boost::python::object package = boost::python::scope() ;
+
+//     package.attr("__path__") = "ostk" ;
+
+//     OpenSpaceToolkitCorePy_Types() ;
+//     OpenSpaceToolkitCorePy_Containers() ;
+//     OpenSpaceToolkitCorePy_FileSystem() ;
+
+// }
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
