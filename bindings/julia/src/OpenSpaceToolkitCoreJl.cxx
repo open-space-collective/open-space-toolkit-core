fd4d9c06cb714d73749c9e0900cdcad59def1920
Remy Derollez <remyderollez@gmail.com>
2021-02-14 11:07:49 -0800

    feat: julia binding code repo structure and initial attempt on Integer class

--- bindings/julia/src/OpenSpaceToolkitCoreJl.cxx
+++ bindings/julia/src/OpenSpaceToolkitCoreJl.cxx
@@ -14,7 +14,11 @@
 #include "jlcxx/jlcxx.hpp"
 #include "jlcxx/functions.hpp"
 
+#include <OpenSpaceToolkit/Core/Types/Integer.hpp>
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using ostk::core::types::Integer ;
 
 // C++ code defining classes
 struct World
@@ -26,13 +30,70 @@ struct World
   ~World() { std::cout << "Destroying World with message " << msg << std::endl; }
 };
 
+// Seems to be needed to mirror types directly without exposing the Integer type
+// template<> struct IsMirroredType<Integer> : std::false_type { };
+
 // C++ code to expose classes and their members to Julia in a Module
-JLCXX_MODULE define_julia_module(jlcxx::Module& CppTypes)
+JLCXX_MODULE OpenSpaceToolkitCore(jlcxx::Module& Core)
 {
-  CppTypes.add_type<World>("World")
+
+  // Dummy Example to add type in the main module directly
+
+  Core.add_type<World>("World")
     .constructor<const std::string&>()
     .method("set", &World::set)
     .method("greet", &World::greet);
+
+  // Adding Integer types to Julia
+
+  Core.add_type<Integer>("Integer")
+
+    // add constructor
+    .constructor<int>()
+
+    // add operators/comparators (cpp function not recognized)
+    // .method("==", &Integer::operator==)
+    // .method("!=", &Integer::operator!=)
+    // .method("<", &Integer::operator<)
+    // .method("<=", &Integer::operator<=)
+    // .method(">", &Integer::operator>)
+    // .method(">=", &Integer::operator>=)
+
+    // .method("+", &Integer::operator==)
+    // .method("+=", &Integer::operator+=)
+    // .method("-", &Integer::operator-)
+    // .method("-=", &Integer::operator-=)
+    // .method("*", &Integer::operator*)
+    // .method("*=", &Integer::operator*=)
+    // .method("/", &Integer::operator/)
+    // .method("/=", &Integer::operator/=)
+
+    // add regular methods
+    .method("is_defined", &Integer::isDefined)
+    .method("is_zero", &Integer::isZero)
+    .method("is_positive", &Integer::isPositive)
+    .method("is_negative", &Integer::isNegative)
+    .method("is_strictly_positive", &Integer::isStrictlyPositive)
+    .method("is_strictly_negative", &Integer::isStrictlyNegative)
+    .method("is_infinity", &Integer::isInfinity)
+    .method("is_positive_infinity", &Integer::isPositiveInfinity)
+    .method("is_negative_infinity", &Integer::isNegativeInfinity)
+    .method("is_finite", &Integer::isFinite)
+    .method("is_even", &Integer::isEven)
+    .method("is_odd", &Integer::isOdd)
+    .method("get_sign", &Integer::getSign)
+    .method("to_string", &Integer::toString) ;
+
+    // add static methods
+
+
+
+  // Adding submodules to main module core [TBR]
+
+  // OpenSpaceToolkitCoreJl_Types(Core) ;
+  // OpenSpaceToolkitCoreJl_Containers(Core) ;
+  // OpenSpaceToolkitCoreJl_FileSystem(Core) ;
+
 }
 
 // BOOST_PYTHON_MODULE (OpenSpaceToolkitCorePy)
