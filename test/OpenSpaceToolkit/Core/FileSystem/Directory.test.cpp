68631e2ba3d445f13254d4b93e143fa0bcfac0bc
Vishwa Shah <vishwa2710@gmail.com>
2023-08-28 20:20:45 +0000

    chore: fix test

--- test/OpenSpaceToolkit/Core/FileSystem/Directory.test.cpp
+++ test/OpenSpaceToolkit/Core/FileSystem/Directory.test.cpp
@@ -351,7 +351,7 @@ TEST(OpenSpaceToolkit_Core_FileSystem_Directory, GetDirectories)
 
         const Array<Directory> subDirectories = directory.getDirectories();
 
-        EXPECT_EQ(2, subDirectories.getSize());
+        EXPECT_EQ(1, subDirectories.getSize());
 
         const Array<String> subDirectoryNames = subDirectories.map<String>(
             [](const Directory& aDirectory) -> String
